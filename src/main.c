46f625ef648a80aac9c30c968859a3cf4048774c
GetTabled <stijnhertog@hotmail.nl>
2022-12-17 23:26:37 +0100

    Fix main.c

--- src/main.c
+++ src/main.c
@@ -6,14 +6,14 @@ static void activate(GtkApplication* app, gpointer user_data) {
   window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "KiwiLab");
   gtk_window_set_default_size(GTK_WINDOW(window), 800, 800);
-  gtk_widget_show_all(window);
+  gtk_widget_show(window);
 }
 
 int main(int argc, char **argv) {
-  GtkApplication* app;
+  GtkApplication *app;
   int status;
 
-  app = gtk_application_new("KiwiLab", G_APPLICATION_DEFAULT_FLAGS);
+  app = gtk_application_new("table.kiwilab", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
